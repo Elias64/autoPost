98c54ee7df71722cf91d29298a1f7aec76d6e5c9
Elias64 <eliasnation@gmail.com>
2016-01-27 14:54:37 -0500

    copyright fixes

--- alifaraji/welcomerobot/event/listener.php
+++ alifaraji/welcomerobot/event/listener.php
@@ -1,8 +1,8 @@
 <?php
 /**
 *
-* @package Welcome Robot
-* @copyright eMosbat & (c) 2014 Ali Faraji(Ali@php) <alifaraji.mail@gmail.com> phpbbpersian.ir
+* @package autoPost
+* @copyright Elias
 * @license GNU General Public License, version 2 (GPL-2.0)
 *
 */
